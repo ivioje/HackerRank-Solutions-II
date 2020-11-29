9244cbfe701510bbc3f69be9657eb8ae07100bc6
Tura <adunola142@gmail.com>
2020-11-29 07:41:02 -0800

    Solutions

---
+++ Day 1/(Functions).js
@@ -0,0 +1,9 @@
+// Day 1: Functions 
+
+/*
+ * Create the function factorial here
+ */
+function factorial(n) {
+    if (n === 0) return 1;
+    else return n * factorial(n - 1)
+}
---
+++ Day 1/Arithmetic Operators.js
@@ -0,0 +1,15 @@
+// Day 1: Arithmetic Operators
+
+function getArea(length, width) {
+    let area;
+    area = length * width
+
+    return area;
+}
+
+function getPerimeter(length, width) {
+    let perimeter;
+    perimeter = (length + width) * 2
+
+    return perimeter;
+}
